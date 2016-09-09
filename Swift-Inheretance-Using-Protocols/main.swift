b33230ef6daa6f2fe23854efd1bfe7787bfae42d
master_cs_se <jose@jortizsd.com>
2016-09-08 18:13:30 -0700

    Initial Commit

---
+++ Swift-Inheretance-Using-Protocols/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Inheretance-Using-Protocols
+//
+//  Created by Master_CS_SE on 9/8/16.
+//  Copyright © 2016 Jose Ortiz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
